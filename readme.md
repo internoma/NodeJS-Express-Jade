971a89330f5d807f8eeefd0373629bb935c3a72b
Alfredo González Portero <alfredo@internoma.com>
2013-06-30 21:47:52 +0200

    ajustes multipágina

--- readme.md
+++ readme.md
@@ -2,4 +2,11 @@
 
 Este es un proyecto de ejercicio para probar NodeJS+Express+Jade.
 
+Uso: $ npm start // levanta la aplicación
+
 Autor: Alfredo González Portero <alfredo@internoma.com>
+
+Historia:
+
+v0.01a [30-06-2013 12:03 Alfredo Gonzalez P. <alfredo@internoma.com>]
+
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,26 @@
 
 /*
- * GET home page.
+ * GET page.
  */
+
+var $autor = 'Alfredo González P.';
+var $date = 2013;
+var $version = '0.0.1a';
 
 exports.index = function(req, res){
   res.render('index', {
-  	title : 'Pruebas con Express',
-  	autor : 'Alfredo González P.',
-  	date  : 2013
+  	title   : 'Pruebas con Express',
+  	autor   : $autor,
+  	date    : $date,
+  	version : $version
+  });
+};
+
+exports.pagina = function(req, res){
+  res.render('pagina', {
+  	title   : 'Pruebas de página con Express',
+  	autor   : $autor,
+  	date    : $date,
+  	version : $version
   });
 };
