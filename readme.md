265094fb85301ef22dbc37ab741c54ce576110f0
Alfredo González Portero <alfredo@internoma.com>
2013-06-30 16:47:33 +0200

    Añadida funcionalidad multipágina

--- readme.md
+++ readme.md
@@ -2,4 +2,6 @@
 
 Este es un proyecto de ejercicio para probar NodeJS+Express+Jade.
 
+Uso: $ npm start // levanta la aplicación
+
 Autor: Alfredo González Portero <alfredo@internoma.com>
--- routes/index.js
+++ routes/index.js
@@ -10,3 +10,11 @@ exports.index = function(req, res){
   	date  : 2013
   });
 };
+
+exports.pagina = function(req, res){
+  res.render('pagina', {
+  	title : 'Pruebas de página con Express',
+  	autor : 'Alfredo González P.',
+  	date  : 2013
+  });
+};
